8edca9f68b7096a4b099db2ae29a9403251f271b
locomatch <38675931+locomatch@users.noreply.github.com>
2020-04-17 17:40:39 -0300

    conexion del cliente y envio de paquetes por socket

--- delibird-commons/delibird-commons/utils/ipc.c
+++ delibird-commons/delibird-commons/utils/ipc.c
@@ -38,25 +38,20 @@ int ipc_escuchar_en(char* ip, char* puerto) {
 }
 
 int ipc_esperar_cliente(int socket_servidor) {
-
 	struct sockaddr_in dir_cliente;
 
 	socklen_t tam_direccion = sizeof(struct sockaddr_in);
-
 	int socket_cliente = accept(socket_servidor, (void*) &dir_cliente, &tam_direccion);
 
 	return socket_cliente;
 }
 
 bool ipc_hay_datos_para_recibir_de(int socket) {
-
 	int buffer;
-
 	return recv(socket, &buffer, sizeof(int), MSG_PEEK) > 0;
 }
 
 t_paquete* ipc_recibir_de(int socket) {
-
 	t_paquete* paquete = malloc(sizeof(t_paquete));
 	t_header* header = malloc(sizeof(t_header));
 
@@ -69,7 +64,47 @@ t_paquete* ipc_recibir_de(int socket) {
 	return paquete;
 }
 
-void ipc_cerrar(int socket) {
+void ipc_enviar(int socket, t_paquete* paquete) {
+	int size = paquete->header->payload_size + sizeof(t_header);
+	void* stream = malloc(size);
+	int offset = 0;
 
+	memcpy(stream+offset, paquete->header, sizeof(t_header));
+	offset += sizeof(t_header);
+	memcpy(stream+offset, paquete->payload, paquete->header->payload_size);
+
+	send(socket, stream, size, 0);
+
+	free(stream);
+}
+
+void ipc_liberar(t_paquete* paquete) {
+	free(paquete->header);
+	free(paquete->payload);
+	free(paquete);
+}
+
+int ipc_conectarse_a(char *ip, char* puerto){
+	struct addrinfo hints;
+	struct addrinfo *server_info;
+
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
+
+	getaddrinfo(ip, puerto, &hints, &server_info);
+
+	int socket_cliente = socket(server_info->ai_family, server_info->ai_socktype, server_info->ai_protocol);
+
+	if(connect(socket_cliente, server_info->ai_addr, server_info->ai_addrlen) == -1)
+		printf("error");
+
+	freeaddrinfo(server_info);
+
+	return socket_cliente;
+}
+
+void ipc_cerrar(int socket) {
  	close(socket);
 }
--- delibird-commons/delibird-commons/utils/ipc.h
+++ delibird-commons/delibird-commons/utils/ipc.h
@@ -25,6 +25,11 @@ int ipc_escuchar_en(char* ip, char* puerto);
 int ipc_esperar_cliente(int socket_servidor);
 bool ipc_hay_datos_para_recibir_de(int socket);
 t_paquete* ipc_recibir_de(int socket);
+
+void ipc_enviar(int socket, t_paquete* paquete);
+int ipc_conectarse_a(char *ip, char* puerto);
+
+void ipc_liberar(t_paquete* paquete);
 void ipc_cerrar(int socket);
 
 #endif //COMUNICATION_H_
