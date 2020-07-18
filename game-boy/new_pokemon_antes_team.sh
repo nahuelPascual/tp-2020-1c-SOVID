5f484595a6f6a83c469f69c4d618e3b5c80c354a
locomatch <38675931+locomatch@users.noreply.github.com>
2020-07-18 03:05:46 -0300

    scripts finales? (#103)
    * :(
    
    * :((

--- game-boy/new_pokemon_antes_team.sh
+++ game-boy/new_pokemon_antes_team.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-./build/game-boy BROKER APPEARED_POKEMON Pikachu 9 9
-./build/game-boy BROKER APPEARED_POKEMON Squirtle 5 2
+./build/game-boy BROKER APPEARED_POKEMON Pikachu 9 9 100
+./build/game-boy BROKER APPEARED_POKEMON Squirtle 5 2 101
 
-./build/game-boy BROKER APPEARED_POKEMON Jolteon 2 2
-./build/game-boy BROKER APPEARED_POKEMON Flareon 4 6
+./build/game-boy BROKER APPEARED_POKEMON Jolteon 2 2 102
+./build/game-boy BROKER APPEARED_POKEMON Flareon 4 6 103
--- game-boy/new_pokemon_post_team.sh
+++ game-boy/new_pokemon_post_team.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-./build/game-boy BROKER APPEARED_POKEMON Onix 2 8
-./build/game-boy BROKER APPEARED_POKEMON Gengar 6 6
-./build/game-boy BROKER APPEARED_POKEMON Squirtle 5 5
+./build/game-boy BROKER APPEARED_POKEMON Onix 2 8 105
+./build/game-boy BROKER APPEARED_POKEMON Gengar 6 6 106
+./build/game-boy BROKER APPEARED_POKEMON Squirtle 5 5 107
 
-./build/game-boy BROKER APPEARED_POKEMON Umbreon 10 6
-./build/game-boy BROKER APPEARED_POKEMON Espeon 7 1
-./build/game-boy BROKER APPEARED_POKEMON Vaporeon 4 10
+./build/game-boy BROKER APPEARED_POKEMON Umbreon 10 6 108
+./build/game-boy BROKER APPEARED_POKEMON Espeon 7 1 109
+./build/game-boy BROKER APPEARED_POKEMON Vaporeon 4 10 110
